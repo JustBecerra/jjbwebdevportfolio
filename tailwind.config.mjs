a7d37478a422d9fbe7affdd625e7f5a5a5744d24
JustBecerra <justj.becerra@gmail.com>
2024-08-01 14:19:29 -0300

    add colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      blue: "#111827",
+      white: "#f9fafb",
+      green: "#239a3b",
+      gray: "#4b5563",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
