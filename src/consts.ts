045ef17cb6dec4c2f978130eb247a28aaf2bd2db
JustBecerra <justj.becerra@gmail.com>
2024-08-01 16:15:31 -0300

    add placeholder names

---
+++ src/consts.ts
@@ -0,0 +1,16 @@
+export type Navtypes = {
+  url: string;
+  logo: string;
+};
+
+export const navlogos = [
+  { url: "mail", logo: "asd" },
+  { url: "linkedin", logo: "asd" },
+  { url: "github", logo: "asd" },
+  { url: "whatsapp", logo: "asd" },
+  {
+    url: "about",
+    logo: "About",
+  },
+  { url: "blog", logo: "Blog" },
+];
