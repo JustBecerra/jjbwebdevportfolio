5029c40900835fcdf2ba81336879ebd74f7d8cf2
JustBecerra <justj.becerra@gmail.com>
2024-08-02 14:37:12 -0300

    add style to buttons

--- src/components/FunctionLogos.tsx
+++ src/components/FunctionLogos.tsx
@@ -6,11 +6,17 @@ const FunctionLogos = () => {
   return (
     <>
       <button className="transparent" onClick={() => setChangeTheme((prev) => !prev)}>
-        {changeTheme === false ? <FaRegSun /> : <FaMoon />}
+        {changeTheme === false ? <FaRegSun className="hover:text-green text-xl" /> : <FaMoon className="hover:text-green text-xl" />}
       </button>
       <button className="transparent" onClick={() => setChangeLanguage((prev) => !prev)}>
-        {changeLanguage === false ? <h1 className="text-base w-[20px]">ES</h1> : <h1 className="text-base w-[20px]">EN</h1>}
+        {changeLanguage === false ? (
+          <h1 className="text-base w-[20px] font-bold font-playfairDisplay hover:text-green">ES</h1>
+        ) : (
+          <h1 className="text-base w-[20px] font-bold font-playfairDisplay hover:text-green">EN</h1>
+        )}
       </button>
+      <button className="transparent font-bold hover:text-green font-playfairDisplay">About</button>
+      <button className="transparent font-bold hover:text-green font-playfairDisplay">Projects</button>
     </>
   );
 };
