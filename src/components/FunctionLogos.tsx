99242ba91da647e410300e7768fc3d9d337bb8a6
JustBecerra <justj.becerra@gmail.com>
2024-08-02 15:51:11 -0300

    add dark mode function and toggle

--- src/components/FunctionLogos.tsx
+++ src/components/FunctionLogos.tsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegSun, FaMoon } from "react-icons/fa";
+
 const FunctionLogos = () => {
   const [changeLanguage, setChangeLanguage] = useState(false);
-  const [changeTheme, setChangeTheme] = useState(false);
+  const [changeTheme, setChangeTheme] = useState("light");
+
+  const handleTheme = () => {
+    if (changeTheme === "light") setChangeTheme("dark");
+    else setChangeTheme("light");
+  };
+
+  useEffect(() => {
+    if (changeTheme === "light") {
+      document.documentElement.classList.remove("dark");
+    } else {
+      document.documentElement.classList.add("dark");
+    }
+    localStorage.setItem("theme", changeTheme);
+  }, [changeTheme]);
 
   return (
     <>
-      <button className="transparent" onClick={() => setChangeTheme((prev) => !prev)}>
-        {changeTheme === false ? <FaRegSun className="hover:text-green text-xl" /> : <FaMoon className="hover:text-green text-xl" />}
+      <button className="transparent" onClick={handleTheme}>
+        {changeTheme === "dark" ? <FaMoon className="hover:text-green text-xl" /> : <FaRegSun className="hover:text-green text-xl" />}
       </button>
       <button className="transparent" onClick={() => setChangeLanguage((prev) => !prev)}>
-        {changeLanguage === false ? (
-          <h1 className="text-base w-[20px] font-bold font-playfairDisplay hover:text-green">ES</h1>
-        ) : (
-          <h1 className="text-base w-[20px] font-bold font-playfairDisplay hover:text-green">EN</h1>
-        )}
+        <h1 className="text-base w-[20px] font-bold font-playfairDisplay dark:text-white text-black hover:text-green">
+          {changeLanguage ? "ES" : "EN"}
+        </h1>
       </button>
       <button className="transparent font-bold hover:text-green font-playfairDisplay">About</button>
       <button className="transparent font-bold hover:text-green font-playfairDisplay">Projects</button>
