bab1956cb17c0d82d16fcf3c13d952347a0b900c
JustBecerra <justj.becerra@gmail.com>
2024-08-02 12:01:51 -0300

    add react and icons

--- src/consts.ts
+++ src/consts.ts
@@ -1,13 +1,18 @@
+import type { IconType } from "react-icons";
+import { FaLinkedin, FaWhatsapp } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+
 export type Navtypes = {
   url: string;
-  logo: string;
+  logo: IconType | string;
 };
 
-export const navlogos = [
-  { url: "mail", logo: "asd" },
-  { url: "linkedin", logo: "asd" },
-  { url: "github", logo: "asd" },
-  { url: "whatsapp", logo: "asd" },
+export const navlogos: Navtypes[] = [
+  { url: "mail", logo: IoMdMail },
+  { url: "linkedin", logo: FaLinkedin },
+  { url: "github", logo: FaGithub },
+  { url: "whatsapp", logo: FaWhatsapp },
   {
     url: "about",
     logo: "About",
