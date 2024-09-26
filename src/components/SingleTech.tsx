09449a78e179e3ad929987fef1db5d751eb1e8c5
JustBecerra <justj.becerra@gmail.com>
2024-09-26 14:54:17 -0300

    add functionality to make it stop

--- src/components/SingleTech.tsx
+++ src/components/SingleTech.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import type { TechandToolsTypes } from "../consts";
+import type { IconType } from "react-icons";
 
 export const SingleTech = ({ logo: Logo, name }: TechandToolsTypes) => {
   const [showName, setShowName] = useState(false);
+
   return (
-    <div onMouseOver={() => setShowName(true)} onMouseLeave={() => setShowName(false)} className="flex flex-col items-center cursor-pointer">
-      <Logo
-        className={`hover:text-green text-5xl dark:hover:text-green text-gray dark:text-white ${showName && "scale-125 transition ease-in-out"}`}
-      />
-      {showName && <p className={`text-green font-bold text-center ${showName && "scale-125 transition ease-in-out"}`}>{name}</p>}
+    <div
+      onMouseOver={() => setShowName(true)}
+      onMouseLeave={() => setShowName(false)}
+      className={`flex flex-col items-center ${showName && "scale-125 transition ease-in-out"} cursor-pointer`}
+    >
+      <Logo className={` text-5xl ${showName && "text-green"} text-gray dark:text-white `} />
+      {showName && <p className={`text-green font-bold text-center `}>{name}</p>}
     </div>
   );
 };
