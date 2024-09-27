478f30993a6ecfb57d9f76fc63cc3a34c4af4139
JustBecerra <justj.becerra@gmail.com>
2024-09-27 10:40:44 -0300

    add mobile design

--- src/components/SingleTech.tsx
+++ src/components/SingleTech.tsx
@@ -11,8 +11,9 @@ export const SingleTech = ({ logo: Logo, name }: TechandToolsTypes) => {
       className={`flex flex-col items-center transition-transform duration-300 ease-in-out cursor-pointer transform hover:scale-125`}
       style={{ transformOrigin: "center" }}
     >
-      <Logo className={` text-5xl ${showName && "text-green"} text-gray dark:text-white `} />
-      {showName && <p className={`text-green text-xs font-bold text-center `}>{name}</p>}
+      <Logo className={`text-5xl ${showName && "lg:text-green"} text-green lg:text-gray dark:text-white`} />
+      {showName && <p className={`text-green text-xs font-bold text-center hidden lg:block`}>{name}</p>}
+      <p className={`text-green text-xs font-bold text-center lg:hidden block`}>{name}</p>
     </div>
   );
 };
