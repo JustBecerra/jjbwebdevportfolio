9317abef7a6b741fab14263e751a527032bf5195
JustBecerra <justj.becerra@gmail.com>
2024-11-02 15:36:13 -0300

    make translate work lets goooo

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,4 +1,9 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
   defaultLocale: 'en',
   locales: ['en', 'es'],
+  load: ['server', 'client'],
+  i18nextServer: {
+    debug: true,
+  },
 };
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import tailwind from '@astrojs/tailwind';
 import astroI18next from 'astro-i18next';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astroI18next()],
+  integrations: [
+    tailwind(),
+    react(),
+    astroI18next({
+      supportedLanguages: ['en', 'es'],
+      defaultLanguage: 'en',
+      i18next: {
+        debug: true,
+        initImmediate: true,
+      },
+    }),
+  ],
 });
