f8b9f8c2189430f8fce6b72fabb67790c5a1c20b
JustBecerra <justj.becerra@gmail.com>
2024-12-16 15:39:57 -0300

    add logic to light up projects when current tech is selected

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,15 +1,17 @@
 import { t } from 'i18next';
 import type { IconType } from 'react-icons/lib';
-
+import { techStore } from '../store';
+import { useEffect, useState } from 'react';
+import { ChallengeInfo, ProfessionalInfo } from '../consts';
 interface ProjectCardProps {
   image: string;
   icon: string;
   name: string;
   type: string;
   description: string;
-  listOfIcons: { logo: IconType; name: string }[];
   appLink: string;
   repoLink: string;
+  typeProject: string;
 }
 
 export const ProjectCard = ({
@@ -18,12 +20,28 @@ export const ProjectCard = ({
   name,
   type,
   description,
-  listOfIcons,
   appLink,
   repoLink,
+  typeProject,
 }: ProjectCardProps) => {
+  const techLogos =
+    typeProject === 'challenge'
+      ? ChallengeInfo.find((info) => info.name === name)?.listOfIcons
+      : ProfessionalInfo.find((info) => info.name === name)?.listOfIcons;
+  const currentTech = techStore((state) => state.tech);
+
+  const compareTech = () => {
+    if (
+      techLogos?.find((element) => element.name === currentTech) !== undefined
+    ) {
+      return true;
+    }
+    return false;
+  };
   return (
-    <div className="w-[350px] lg:w-[400px] h-[600px] rounded-md flex flex-col gap-8 bg-gray hover:scale-110 transition ease-in-out delay-150">
+    <div
+      className={`w-[350px] lg:w-[400px] ${compareTech() && 'shadow-2xl shadow-green'} h-[600px] rounded-md flex flex-col gap-8 bg-gray hover:scale-110 transition ease-in-out delay-150`}
+    >
       <div className="w-full h-fit">
         <img
           src={image}
@@ -46,11 +64,12 @@ export const ProjectCard = ({
       <div className="flex flex-col items-center gap-4 h-[200px]">
         <h3 className="text-green font-bold">{t('projects.tech')}</h3>
         <div className="flex text-3xl lg:text-4xl gap-4 text-green">
-          {listOfIcons.map(({ name, logo: Logo }) => (
-            <div key={name} id={name}>
-              <Logo width={24} height={24} size={24} />
-            </div>
-          ))}
+          {techLogos &&
+            techLogos.map(({ name, logo: Logo }, index) => (
+              <div key={`${name}-${index}`} id={name}>
+                <Logo width={24} height={24} size={24} />
+              </div>
+            ))}
         </div>
         <div className="mt-6 flex gap-8">
           <a
