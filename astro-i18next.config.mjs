9608a0ec72f1c50b34966ab8546df0f897cb8725
JustBecerra <justj.becerra@gmail.com>
2025-03-05 17:07:52 -0300

    revert routes

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -4,15 +4,6 @@ export default {
   locales: ['en', 'es'],
   load: ['server', 'client'],
   i18nextServer: {
-    debug: process.env.NODE_ENV === 'development', // Enable debug only in development
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // Correct relative path for production
-    },
-  },
-  i18nextClient: {
-    debug: process.env.NODE_ENV === 'development', // Enable debug only in development
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // Correct relative path for production
-    },
+    debug: true,
   },
 };
